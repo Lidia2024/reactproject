4084fe760e7fc1fcf0d6a215bcb7d9ca3cb09823
Lidia2024 <lida.kurgan200042@gmail.com>
2024-03-06 19:02:45 +0300

    Clear project directory and add components
    Clear project in public and src. and add directory 'components' with added compomemts Header and Footer

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+
+export default function Footer(){
+    return(
+        <footer>
+            <div>
+                Все правила защищены &copy;
+            </div>
+        </footer>
+    );
+}
