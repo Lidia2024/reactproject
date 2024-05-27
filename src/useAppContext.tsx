e31064ed68140f02fae1f16189a82b2dc543784a
Lidia2024 <lida.kurgan200042@gmail.com>
2024-05-27 20:18:46 +0300

    Shop items

--- src/useAppContext.tsx
+++ src/useAppContext.tsx
@@ -1,4 +1,7 @@
 import React,{useState, useEffect, useContext} from "react";
+import axios from "axios";
+import { parseJsonSourceFileConfigFileContent } from "typescript";
+
 const AppContext=React.createContext<any>(null);
 
 
@@ -12,124 +15,24 @@ export const useAppContext= () => {
 }
 
 const AppProvider:React.FC<{children:React.ReactNode}>=({children})=>{
-    const [items,setItems]=useState<any[]>([
-        {
-          id:1,
-          title:'Ruler',
-          img:'Линейка.jpg',
-          desc:'Plastic mulricolored ruler',
-          category:'Office supplies',
-          price:'2.99$'
-    
-        },
-        {
-          id:2,
-          title:'Pencil',
-          img:'простой.jpg',
-          desc:'Gray pencil kit, HB hardness',
-          category:'Office supplies',
-          price:'4.99$'
-    
-        },
-        {
-          id:3,
-          title:'Pen',
-          img:'ручка.jpg',
-          desc:'Blue ball-point pen kit',
-          category:'Office supplies',
-          price:'7.99$'
-    
-        },
-        {
-          id:4,
-          title:'Notebook',
-          img:'Тетрадь.jpeg',
-          desc:'Mulricolored 12 page quad-ruled notebook',
-          category:'Office supplies',
-          price:'3.99$'
-    
-        },
-        {
-          id:5,
-          title:'Calculator',
-          img:'калькуль.jpg',
-          desc:'Plastic mulricolored calculator',
-          category:'Office supplies',
-          price:'10.99$'
-    
-        },
-        {
-          id:6,
-          title:'Globe',
-          img:'глобус.jpg',
-          desc:'Plastic coloured 70sm globe',
-          category:'Decor',
-          price:'20.99$'
-    
-        },
-        {
-          id:7,
-          title:'Eraser',
-          img:'стерка.jpg',
-          desc:'Soft&Hard side two-colored eraser',
-          category:'Office supplies',
-          price:'2.99$'
-    
-        },
-        {
-          id:8,
-          title:'Book-mark',
-          img:'закладки.jpg',
-          desc:'Sticky multicolored paper book-mark kit',
-          category:'Office supplies',
-          price:'5.99$'
-    
-        },
-        {
-          id:9,
-          title:'Pensil-case',
-          img:'пенал.jpg',
-          desc:'Pastel purple case',
-          category:'Storage',
-          price:'12.99$'
-    
-        },
-        {
-          id:10,
-          title:'Folder',
-          img:'папка.jpg',
-          desc:'Plastic mulricolored folder',
-          category:'Storage',
-          price:'7.99$'
-    
-        },
-        {
-          id:11,
-          title:'Pencil Sharpener',
-          img:'Точилка.jpg',
-          desc:'Stainless steel blade plasric pencil sharpener',
-          category:'Office supplies',
-          price:'15.99$'
-    
-        },
-        {
-          id:12,
-          title:'Compass',
-          img:'Циркуль.jpg',
-          desc:'Metal multipurpose mathimatical compass',
-          category:'Office supplies',
-          price:'14.99$'
-    
-        }
-    ]);
+    const [items,setItems]=useState<any[]>([]);
     const [showFullItem,setShowFullItem]=useState <boolean>(false);
     const[orders,setOrders]=useState <any[]>([]);
     const [currentItems,setCurrentItems]=useState <any[]>([]);
     const[fullItem, setFullItem]=useState <any>({});
 
     useEffect(()=>{
-      setCurrentItems(items);
-    },[items]);
+      axios
+        .get("http://localhost:3001/items")
+        .then((responce)=>{
+          setItems(responce.data);
+          chooseCategory("all");
+          setCurrentItems(responce.data);
+        })
+        .catch((error)=>{
+          console.log("Data loading error",error);
+        });
+    },[]);
   
     const deleteOrder=(id:number)=>{
         setOrders(orders.filter((el)=> el.id!==id));
